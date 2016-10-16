198fbcab8482cf5d55abab2eacbbc14759bdde43
Binoy Jayan <binoy.jayan@linaro.org>
2016-10-16 18:43:29 +0530

    crypto: Add IV generation algorithms
    Currently, the iv generation algorithms are implemented in dm-crypt.c.
    The goal is to move these algorithms from the dm layer to the kernel
    crypto layer by implementing them as template ciphers so they can be used
    in relation with algorithms like aes, and with multiple modes like cbc,
    ecb etc. As part of this patchset, the iv-generation code is moved from the
    dm layer to the crypto layer. The dm-layer can later be optimized to
    encrypt larger block sizes in a single call to the crypto engine. The iv
    generation algorithms implemented in geniv.c includes plain, plain64,
    essiv, benbi, null, lmk and tcw. These templates are to be configured
    and has to be invoked as:
    
    crypto_alloc_skcipher("plain(cbc(aes))", 0, 0);
    crypto_alloc_skcipher("essiv(cbc(aes))", 0, 0);
    ...
    
    from the dm layer.
    
    Signed-off-by: Binoy Jayan <binoy.jayan@linaro.org>

--- include/crypto/skcipher.h
+++ include/crypto/skcipher.h
@@ -87,6 +87,7 @@ struct crypto_skcipher {
  *	    length for validity. In case a software fallback was put in place in
  *	    the @cra_init call, this function might need to use the fallback if
  *	    the algorithm doesn't support all of the key sizes.
+ * @setkey: Set context data for the cipher
  * @encrypt: Encrypt a scatterlist of blocks. This function is used to encrypt
  *	     the supplied scatterlist containing the blocks of data. The crypto
  *	     API consumer is responsible for aligning the entries of the
@@ -122,6 +123,8 @@ struct crypto_skcipher {
 struct skcipher_alg {
 	int (*setkey)(struct crypto_skcipher *tfm, const u8 *key,
 	              unsigned int keylen);
+	int (*set_ctx)(struct crypto_skcipher *tfm, void *ctx,
+		       unsigned int len);
 	int (*encrypt)(struct skcipher_request *req);
 	int (*decrypt)(struct skcipher_request *req);
 	int (*init)(struct crypto_skcipher *tfm);
@@ -366,6 +369,21 @@ static inline int crypto_skcipher_setkey(struct crypto_skcipher *tfm,
 {
 	return tfm->setkey(tfm, key, keylen);
 }
+/**
+ * crypto_skcipher_set_ctx() - set initial context for cipher
+ * @tfm: cipher handle
+ * @ctx: buffer holding the context data
+ * @len: length of the context data structure
+ *
+ */
+static inline void crypto_skcipher_set_ctx(struct crypto_skcipher *tfm,
+					 void *ctx, unsigned int len)
+{
+	struct skcipher_alg *alg = crypto_skcipher_alg(tfm);
+
+	alg->set_ctx(tfm, ctx, len);
+}
+
 
 static inline bool crypto_skcipher_has_setkey(struct crypto_skcipher *tfm)
 {
